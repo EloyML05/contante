d693be8534b884d28ef8638bafb8c71e8225004c
EloyML05 <eloymitalopez3@gmai.com>
2024-10-23 18:13:10 +0200

    first commit

---
+++ contante-backend/src/main/java/net/ausiasmarch/contante/exception/ResourceNotEditException.java
@@ -0,0 +1,7 @@
+package net.ausiasmarch.contante.exception;
+
+public class ResourceNotEditException extends RuntimeException{
+    public ResourceNotEditException(String mensaje){
+        super(mensaje);
+    }
+}
